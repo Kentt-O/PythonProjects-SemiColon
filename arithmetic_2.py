43e5d696d5f8a8d3e28d035d801470134d9b1340
Kentt-O <kentc.okoro@gmail.com>
2024-04-15 10:34:40 +0100

    add task

---
+++ arithmetic_2.py
@@ -0,0 +1,11 @@
+# (Arithmetic) For each of the arithmetic operators +, -, *, /, // and **, display the value of an expression with # # 27.5 as the left operand and 2 as the right operand.
+
+digit1 = 27.5
+digit2 = 2
+
+print(digit1 + digit2)
+print(digit1 - digit2)
+print(digit1 * digit2)
+print(digit1 / digit2)
+print(digit1 // digit2)
+print(digit1 ** digit2)
